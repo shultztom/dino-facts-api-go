3b03d5888b955fa14820fb7089e246713da96553
shultztom <shultzthomask@sau.edu>
2022-05-07 09:40:04 -0500

    moved to gcp registry

--- dockerBuildAndPush.sh
+++ dockerBuildAndPush.sh
@@ -1,2 +1,2 @@
-docker build -t shultzlab.jfrog.io/default-docker-virtual/shultztom/dino-facts-api-go:latest .
-docker push shultzlab.jfrog.io/default-docker-virtual/shultztom/dino-facts-api-go:latest
+docker build -t us-central1-docker.pkg.dev/shultzlab/docker-gcp/dino-facts-api-go:latest .
+docker push us-central1-docker.pkg.dev/shultzlab/docker-gcp/dino-facts-api-go:latest
